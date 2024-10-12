3ae631313d3a75ec6a72e70a80c6a8fecb6c50ab
RyderLangston <Ryderlangston258@outlook.com>
2024-10-12 16:51:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-function login() {
-    const employeeId = document.getElementById('employeeId').value;
-
-    // Check if the employee ID is valid
-    if (employeeId === '162721' || employeeId === '834868' || employeeId === '091827') {
-        localStorage.setItem('currentUser', JSON.stringify({ id: employeeId }));
-        window.location.href = 'dashboard.html'; // Redirect to the dashboard
-    } else {
-        alert('Invalid Employee ID. Please try again.');
-    }
+function goTo(page) {
+    window.location.href = page; // Redirect to the specified page
 }
